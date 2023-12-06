f6f2f2c3a48f1a364e1f5cb24521dd32fdd734a5
Cesar <julio45321@gmail.com>
2023-12-06 16:48:25 -0600

    Completado

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-//swiper
-var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
